5c312f76715a5e2dcd8a64c9253847e76c247538
Francisco Desktop <fjtejerina@gmail.com>
2025-09-23 23:41:11 -0400

    Comprobar la funcion de obtener un nombre

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades
 //  en lógica de programación. Aquí deberás desarrollar la lógica para
 //  resolver el problema.
+let input = document.getElementById('amigo');
+let valorIngresado = input.value;
+
+console.log(valorIngresado);
