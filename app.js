3f74f57700a6f4960e62b2787293745ec07a3692
Francisco Desktop <fjtejerina@gmail.com>
2025-09-24 02:07:31 -0400

    Ultima actualizacion

--- app.js
+++ app.js
@@ -11,25 +11,52 @@ let mensajeBoton = "";
 function agregarAmigo(){
     let input = document.getElementById('amigo');
     let valorIngresado = input.value;
-    console.log(valorIngresado);
-    listaAmigos.push(valorIngresado);
-    console.log(listaAmigos);
+    if (valorIngresado=="") {
+        alert("Ingrese un nombre valido");
+    }else{
+        console.log(valorIngresado);
+        listaAmigos.push(valorIngresado);
+        console.log(listaAmigos);
+        let ul = document.getElementById('listaAmigos');
+        ul.innerHTML ="";
+
+        for (let index = 0; index < listaAmigos.length; index++) {
+            let amigo = document.createElement("li");
+            amigo.innerHTML = "";
+            amigo.textContent = listaAmigos[index];
+            ul.appendChild(amigo);
+        }
+        input.value = "";
+    }
+
 }
 
 function sortearAmigo(){
     
-    let numeroAleatorio = Math.floor(Math.random()*listaAmigos.length);
-    let nombreAmigoSecreto = document.querySelector('h2')
-    nombreAmigoSecreto.innerHTML = `Tu amigo secreto es ${listaAmigos[numeroAleatorio]}`;
-    console.log (numeroAleatorio)
-    soloUnClick();
+   if (listaAmigos != "") {
+        let numeroAleatorio = Math.floor(Math.random()*listaAmigos.length);
+    
+        let ul2 = document.getElementById('resultado');
+        ul2.innerHTML = `Tu amigo secreto es ${listaAmigos[numeroAleatorio]}`;
+        console.log (numeroAleatorio)
+        soloUnClick();
+   }else{
+    alert("No hay nombres para sortear");
+   }
 }
 
 function soloUnClick(){
-    botonPresionado = true;
+    
     console.log('Botón presionado por primera vez');
     mensajeBoton = document.getElementById('sorteo').innerHTML =
         `<img src="assets/play_circle_outline.png" alt="Ícono para sortear"> Ya tienes un Amigo Secreto`;
     document.getElementById('sorteo').style.backgroundColor = '#D3D3D3';
     document.getElementById('sorteo').disabled = true;
+}
+
+function validarAmigo(){
+    if (!botonPresionado) {
+        botonPresionado = true;
+        alert("Ingrese un nombre valido");
+    } 
 }
