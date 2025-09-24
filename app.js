3ae6b60cace8e0a6c3868d808149770e5aced1b0
Francisco Desktop <fjtejerina@gmail.com>
2025-09-24 00:29:54 -0400

    Funciones del juego creadas

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades
 //  en lógica de programación. Aquí deberás desarrollar la lógica para
 //  resolver el problema.
-let input = document.getElementById('amigo');
-let valorIngresado = input.value;
+let input = 0;
+let valorIngresado = 0;
+let listaAmigos = [];
+let numeroAleatorio = 0;
+let botonPresionado = false;
 
-console.log(valorIngresado);
+function agregarAmigo(){
+    let input = document.getElementById('amigo');
+    let valorIngresado = input.value;
+    console.log(valorIngresado);
+    listaAmigos.push(valorIngresado);
+    console.log(listaAmigos);
+}
+
+function sortearAmigo(){
+    
+    let numeroAleatorio = Math.floor(Math.random()*listaAmigos.length);
+    let nombreAmigoSecreto = document.querySelector('h2')
+    nombreAmigoSecreto.innerHTML = `Tu amigo secreto es ${listaAmigos[numeroAleatorio]}`;
+    console.log (numeroAleatorio)
+}
+
+
